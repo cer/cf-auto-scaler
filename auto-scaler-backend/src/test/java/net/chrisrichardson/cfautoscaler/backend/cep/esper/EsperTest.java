3565959c17ec25037aeaa65a5f472cedc382649e
Chris Richardson <chris@chrisrichardson.net>
2013-02-05 09:52:41 -0800

    A few more EsperTests

--- auto-scaler-backend/src/test/java/net/chrisrichardson/cfautoscaler/backend/cep/esper/EsperTest.java
+++ auto-scaler-backend/src/test/java/net/chrisrichardson/cfautoscaler/backend/cep/esper/EsperTest.java
@@ -12,6 +12,7 @@ import net.chrisrichardson.cfautoscaler.backend.cep.ApplicationAlarmConsumer;
 import net.chrisrichardson.cfautoscaler.backend.collection.InstanceMeasurement;
 
 import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.espertech.esper.client.time.CurrentTimeEvent;
@@ -23,9 +24,10 @@ public class EsperTest {
   private static final String APP_NAME = "myapp";
   private Esper esper;
   private DateTime startTime;
+  private ApplicationAlarmConsumer applicationAlarmConsumer;
 
-  @Test
-  public void esperShouldProcessEvents() throws InterruptedException {
+  @Before
+  public void initialize() {
     startTime = new DateTime();
 
     esper = new Esper() {
@@ -35,27 +37,68 @@ public class EsperTest {
       };
     };
     esper.initializeEsper();
+    applicationAlarmConsumer = mock(ApplicationAlarmConsumer.class);
+  }
+
+  @Test
+  public void esperShouldGenerateIdleAlarm() throws InterruptedException {
+
+    String alarmName = "idle";
+   
+    registerAlarmAndSubscribe(alarmName, AlarmOperator.LT, 0.3);
+
+    publishMeasurements(0.1);
+
+    verifyAlarmGenerated(alarmName);
+  }
+
+  @Test
+  public void esperShouldGenerateBusyAlarm() throws InterruptedException {
     
-    ApplicationAlarmConsumer applicationAlarmConsumer = mock(ApplicationAlarmConsumer.class);
+    String alarmName = "busy";
+    registerAlarmAndSubscribe(alarmName, AlarmOperator.GT, 0.8);
+    
+    publishMeasurements(0.9);
+    
+    verifyAlarmGenerated(alarmName);
+  }
 
-    esper.registerAlarm(APP_NAME, "idle", new AlarmSpec(METRIC_NAME, AlarmOperator.LT, 0.3));
-    esper.subscribeToAlarms(APP_NAME, applicationAlarmConsumer);
-
-    int interval = Esper.AVERAGE_WINDOW_SIZE * 2;
-    for (int time = 1; time <= interval; time++) {
-      timeNow(time);
-      lowCpu();
-    }
-    timeNow(interval + 1);
-
+  @Test
+  public void esperShouldNotGenerateAnyAlarms() throws InterruptedException {
+    
+    registerAlarmAndSubscribe("idle", AlarmOperator.LT, 0.3);
+    registerAlarmAndSubscribe("busy", AlarmOperator.GT, 0.8);
+    
+    publishMeasurements(0.5);
+    
+    verify(applicationAlarmConsumer, times(4)).processMetric(eq(METRIC_NAME), anyDouble());
+    verifyNoMoreInteractions(applicationAlarmConsumer);
+  }
+  
+  private void verifyAlarmGenerated(String alarmName) {
     verify(applicationAlarmConsumer, times(2)).processMetric(eq(METRIC_NAME), anyDouble());
-    verify(applicationAlarmConsumer).processAlarm("idle");
-
+    verify(applicationAlarmConsumer).processAlarm(alarmName);
     verifyNoMoreInteractions(applicationAlarmConsumer);
   }
 
-  private void lowCpu() {
-    InstanceMeasurement event = new InstanceMeasurement(APP_NAME, "1", METRIC_NAME, 0.1);
+  
+
+  private void registerAlarmAndSubscribe(String alarmName, AlarmOperator operator, double threshold) {
+    esper.registerAlarm(APP_NAME, alarmName, new AlarmSpec(METRIC_NAME, operator, threshold));
+    esper.subscribeToAlarms(APP_NAME, applicationAlarmConsumer);
+  }
+
+  private void publishMeasurements(double value) {
+    int interval = Esper.AVERAGE_WINDOW_SIZE * 2;
+    for (int time = 1; time <= interval; time++) {
+      timeNow(time);
+      cpu(value);
+    }
+    timeNow(interval + 1);
+  }
+
+  private void cpu(double value) {
+    InstanceMeasurement event = new InstanceMeasurement(APP_NAME, "1", METRIC_NAME, value);
     esper.publish(event);
   }
 
